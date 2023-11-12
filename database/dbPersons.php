ee7a86b66c55968e6b0c5ca1e2a2656aa9ceb0b4
jcochran840 <jcochran840@gmail.com>
2023-11-12 00:40:53 -0500

    test

--- database/dbPersons.php
+++ database/dbPersons.php
@@ -622,14 +622,14 @@ function get_logged_hours($from, $to, $name_from, $name_to, $venue) {
 
     //custom version of find user
     function find_fbank($name = null, $zip = null, $tag = null, $county = null) {
-        var_dump($name);
-        var_dump($county);
+        //var_dump($name);
+        //var_dump($county);
         //var_dump($tag);
         //var_dump($zip);
         $where = 'where ';
       
         if ($name !== null) {
-            var_dump($name);
+            //var_dump($name);
           $where .= "first_name like '%$name%'";
         }
       
@@ -648,7 +648,7 @@ function get_logged_hours($from, $to, $name_from, $name_to, $venue) {
         }
       
         if ($county !== null) {
-            //var_dump($county);
+            var_dump($county);
           if (!empty($where)) {
             $where .= ' and ';
           }
