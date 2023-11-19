24953d8e2a453e94431287a37f4192fbde6eb196
jcochran840 <jcochran840@gmail.com>
2023-11-19 01:26:07 -0500

    replaced gwyneth gift's logo

--- header.php
+++ header.php
@@ -106,8 +106,8 @@
         }
         else {
             echo('<nav>');
-            echo('<span id="nav-top"><span class="logo"><a class="navbar-brand" href="' . $path . 'index.php"><img src="images/gwynethsgift.png"></a>');
-            echo('<a class="navbar-brand" id="vms-logo">VMS</a></span><img id="menu-toggle" src="images/menu.png"></span>');
+            echo('<span id="nav-top"><span class="logo"><a class="navbar-brand" href="' . $path . 'index.php"><img src="images/Food_Services.png"></a>');
+            echo('<a class="navbar-brand" id="vms-logo"></a></span><img id="menu-toggle" src="images/menu.png"></span>');
             echo('<ul>');
             //echo " <br><b>"."Gwyneth's Gift Homebase"."</b>|"; //changed: 'Homebase' to 'Gwyneth's Gift Homebase'
 	        if ($_SESSION['access_level'] >= 1) {
